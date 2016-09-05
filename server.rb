e6ce2ad02615881e0c6546ed278b07b8432dbd26
rhys <rbrooker105@gmail.com>
2016-09-05 22:05:46 +1000

    GET response successful from server

--- server.rb
+++ server.rb
@@ -1,11 +1,27 @@
 require 'socket'               # Get sockets from stdlib
+require 'json'
 
 server = TCPServer.open(2000)  # Socket to listen on port 2000
 loop {                         # Servers run forever
   client = server.accept      # Wait for a client to connect
-  web = client.gets.chomp
-  if web == "GET /index.html HTTP/1.0"
-  	client.puts File.open("index.html").readlines
+  request = client.gets
+  puts request
+  if request =~ (/GET(.*)/ )
+  	page = request.split(" ")[1][1..-1] #Get middle word of request, remove leading '/'
+  	if File.file?(page)
+  	  lines = File.open(page).readlines
+  	    client.puts "HTTP/1.0 200 OK"
+  	  	client.puts "Date: #{Time.now}"
+  	  	client.puts "Content-Type: text/html"
+  	  	client.puts "Content-Length: #{lines.join.size}\r\n\r\n\ " #Server html reply
+     	client.puts lines
+  	else
+  	  client.puts "HTTP/1.0 404 Not Found \r\n\r\n "
+  	  client.puts "Error 404, file not found!"
+  	end
+  	if request =~ (/POST(.*)/ )
+  	 puts request
+  	end
   end
 
   #client.puts(Time.now.ctime)  # Send the time to the client
--- tiny_browser.rb
+++ tiny_browser.rb
@@ -1,15 +1,44 @@
 require 'socket'
+require 'json'
+
  
 host = 'localhost'     # The web server
 port = 2000                           # Default HTTP port
 path = "/index.html"                 # The file we want 
 
 # This is the HTTP request we send to fetch a file
-request = "GET #{path} HTTP/1.0\r\n\r\n"
 
+puts "Would you like to POST or GET to the page?(post/get)"
+option = gets.chomp
+if option == "get"
+	puts "get"
+  request = "GET #{path} HTTP/1.0\r\n\r\n"
+elsif option == "post"
+  puts "Pretend you're registering a viking for a raid."
+  puts "What's your viking name?"
+  viking_name = gets.chomp
+  puts "What's your viking email?"
+  viking_email = gets.chomp
+  viking_hash = {:viking => {:name=>viking_name,:email=>viking_email}}
+  request = ["POST thanks.html HTTP/1.0\r\n\r\n",
+ 			"User-Agent: HTTPTool/1.0",
+			"Content-Type: application/x-www-form-urlencoded",
+			"Content-Length: #{viking_hash.to_json.length}",
+			viking_hash.to_json]
+#else
+  #puts "Not a valid option"
+end
+
+
+puts request
 socket = TCPSocket.open(host,port)  # Connect to server
 socket.print(request)               # Send request
 response = socket.read              # Read complete response
 # Split response at first blank line into headers and body
+# Split response at first blank line into headers and body
 headers,body = response.split("\r\n\r\n", 2) 
-print response#body                          # And display it
+#print body                          # And display it
+puts "headers"
+puts headers
+puts "and body"
+puts body
