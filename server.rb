1ea9f1bfa8b5e3d963c11b559859c3e4036159e7
rhys <rbrooker105@gmail.com>
2016-09-09 14:21:54 +1000

    Server replying to post request successfully

--- server.rb
+++ server.rb
@@ -1,30 +1,65 @@
 require 'socket'               # Get sockets from stdlib
 require 'json'
 
+
+
 server = TCPServer.open(2000)  # Socket to listen on port 2000
 loop {                         # Servers run forever
   client = server.accept      # Wait for a client to connect
-  request = client.gets
-  puts request
+  request = ""
+  
+
+  while line = client.gets
+    request << line
+    break if request =~ /\r\n\r\n$/
+  end
+  
+
+  page = request.split(" ")[1][1..-1] #Get middle word of request, remove leading '/' e.g. index.html
+  
+
   if request =~ (/GET(.*)/ )
-  	page = request.split(" ")[1][1..-1] #Get middle word of request, remove leading '/'
   	if File.file?(page)
   	  lines = File.open(page).readlines
-  	    client.puts "HTTP/1.0 200 OK"
-  	  	client.puts "Date: #{Time.now}"
-  	  	client.puts "Content-Type: text/html"
-  	  	client.puts "Content-Length: #{lines.join.size}\r\n\r\n\ " #Server html reply
-     	client.puts lines
+      client.puts "HTTP/1.0 200 OK"
+      client.puts "Date: #{Time.now}"
+      client.puts "Content-Type: text/html"
+      client.puts "Content-Length: #{lines.join.size}\r\n\r\n\ " #Server html reply
+      client.puts lines
   	else
   	  client.puts "HTTP/1.0 404 Not Found \r\n\r\n "
-  	  client.puts "Error 404, file not found!"
+      client.puts "Error 404, file not found!"
   	end
-  	if request =~ (/POST(.*)/ )
-  	 puts request
-  	end
+  end
+  if request =~ (/POST(.*)/ )
+    puts request
+    header,data = request.split("\r\n", 2)
+    data = data.chomp.chomp[1..-1]
+    params = Hash.new
+    params = JSON.parse(data)
+    list_entries = ""
+    params["viking"].each do |key, value|
+      list_entries += "<li>#{key}: #{value}</li>\n      "
+    end
+    if File.file?(page)
+      file = File.open(page)
+      lines = file.read.gsub("<%= yield %>", list_entries)
+      client.puts "HTTP/1.0 200 OK"
+      client.puts "Date: #{Time.now}"
+      client.puts "Content-Type: text/html"
+      client.puts "Content-Length: #{lines.length}\r\n\r\n\ " #Server html reply
+      client.puts lines
+    else
+      client.puts "HTTP/1.0 404 Not Found \r\n\r\n "
+      client.puts "Error 404, file not found!"
+    end
+
   end
 
   #client.puts(Time.now.ctime)  # Send the time to the client
   #client.puts "Closing the connection. Bye!"
+  
+
   client.close                 # Disconnect from the client
 }
+
--- tiny_browser.rb
+++ tiny_browser.rb
@@ -1,32 +1,34 @@
 require 'socket'
 require 'json'
-
  
 host = 'localhost'     # The web server
-port = 2000                           # Default HTTP port
-path = "/index.html"                 # The file we want 
+port = 2000            # Default HTTP port
 
-# This is the HTTP request we send to fetch a file
-
+#Get user input before we send the request to the server
 puts "Would you like to POST or GET to the page?(post/get)"
 option = gets.chomp
-if option == "get"
-	puts "get"
-  request = "GET #{path} HTTP/1.0\r\n\r\n"
-elsif option == "post"
+puts "Whats is the name of the file you would like to retrieve? (index.html for get and thanks.html for post)"
+path = gets.chomp
+
+
+if option == "post"
   puts "Pretend you're registering a viking for a raid."
   puts "What's your viking name?"
   viking_name = gets.chomp
   puts "What's your viking email?"
   viking_email = gets.chomp
   viking_hash = {:viking => {:name=>viking_name,:email=>viking_email}}
-  request = ["POST thanks.html HTTP/1.0\r\n\r\n",
- 			"User-Agent: HTTPTool/1.0",
-			"Content-Type: application/x-www-form-urlencoded",
-			"Content-Length: #{viking_hash.to_json.length}",
-			viking_hash.to_json]
-#else
-  #puts "Not a valid option"
+  viking_json = viking_hash.to_json
+  request = <<POST_REQUEST
+POST /#{path} HTTP/1.0
+From: frog@jmarshall.com
+Content-Type: application/json
+Content-Length: #{viking_json.length}
+\r\n #{viking_json}\r\n\r\n
+POST_REQUEST
+
+else  #We will just get the #{path} if there's an error in option variable input
+  request = "GET /#{path} HTTP/1.0\r\n\r\n"
 end
 
 
@@ -37,8 +39,7 @@ response = socket.read              # Read complete response
 # Split response at first blank line into headers and body
 # Split response at first blank line into headers and body
 headers,body = response.split("\r\n\r\n", 2) 
-#print body                          # And display it
-puts "headers"
 puts headers
-puts "and body"
+puts ""
+puts "" #separate headers from body
 puts body
